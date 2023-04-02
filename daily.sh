adfefbe894dbb5969a552ac1556136fa2d321fe6
Steven Van Ingelgem <steven@vaningelgem.be>
2023-04-02 08:42:02 +0200

    Fix daily script

--- daily.sh
+++ daily.sh
@@ -5,6 +5,5 @@ cd "$(dirname "$0")"
 /opt/anaconda3/bin/python src/download.py
 
 docker_id=$(docker ps | grep "learn_english" | cut -f1 -d" ")
-docker container stop $docker_id
+docker container restart $docker_id
 
-./run.sh
